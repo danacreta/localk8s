271bc93b92ffe9b4c6a98b60297db83fcb91e8a9
danacreta <dana.creta@gmail.com>
2021-09-27 18:19:20 +0000

    bash script to call yml scripts

---
+++ local_k8s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#if [[ $# -ne 1 && $1 == @(create_cluster|deploy_application|destroy_cluster) ]]; then
+if [ "$#" -ne 1 -o \( "$1" != "create_cluster" -a "$1" != "deploy_application" -a "$1" != "destroy_cluster" \) ]; then
+    echo "Please specify an action: create_cluster | deploy_application | destroy_cluster"
+    exit 1
+else
+    #output=$(eval "ansible-playbook k8s/$1.yml")
+    eval "ansible-playbook k8s/$1.yml"
+    #echo "$output"
+fi
